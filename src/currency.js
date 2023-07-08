866e283c30148345b7ad8e8e6f5d982e66145220
JaseGrable <jase.grable@gmail.com>
2023-07-07 19:23:13 -0700

    Update currency converter API logic

--- src/currency.js
+++ src/currency.js
@@ -1,1 +1,16 @@
-export class CurrencyConverter {}
+export class CurrencyConverter {
+    static async converter(inputUSD, foreignCurrency){
+        return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${foreignCurrency}/${inputUSD}`)
+        .then(function(response){
+            if(!response.ok) {
+                let errorResponse = `${response.status} ${response.statusText}`;
+                throw new Error(errorResponse);
+            } else {
+                return response.json();
+            }
+        })
+        .catch(function (error){
+            return error; 
+        });
+    }
+}
